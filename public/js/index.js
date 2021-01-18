27e32177d6e5262bfc0d20ae7b3f3202a1a67df1
birdmoon <birdmoon@nestory.com>
2021-01-19 00:58:21 +0900

    js로 scrollHeight 적용까지

---
+++ public/js/index.js
@@ -0,0 +1,56 @@
+(() => {
+  const sceneInfo = [
+    {
+      //0
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      //1
+      type: "normal",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      //2
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      //3
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    console.log(sceneInfo);
+  }
+  window.addEventListener("resize", setLayout);
+  setLayout();
+})();
+//즉시호출함수 이렇게 하면 함수가 자동으로 호출이 됨
+//즉시호출함수는 지역변수로 활용을 하여 바깥에서 접근하지 못하는 장점이 있다.
