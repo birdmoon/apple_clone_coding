3b9f18670c5cd25aba8dfb4fcef861613382abf4
birdmoon <birdmoon@nestory.com>
2021-01-20 22:46:10 +0900

    섹션별 구분 높이 scrollLoop()펑션 적용

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 (() => {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이 값의 합.
+  let currentScene = 0; // 현재 활성화 된 (모니터에 보이는) Scene(scroll-section)
+
   const sceneInfo = [
     {
       //0
@@ -46,10 +50,37 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-
-    console.log(sceneInfo);
   }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    // for (let i = 0; i < currentScene; i++) {
+    //   prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
+    //   //스크롤 될 때마다 scene높이값이 누적되므로 prevScrollHeight를 더해주는거임
+    // }
+
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return; //초기 상태에서 스크롤을 위로 올릴경우 currentScene이 마이너스가 될 수 있으므로 안전장치 걸어두는 게 좋다.
+      currentScene--;
+    }
+
+    console.log(currentScene);
+  }
+
   window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset; //scroll할때마다 yOffset값이 매겨진다.
+    scrollLoop();
+  });
+
   setLayout();
 })();
 //즉시호출함수 이렇게 하면 함수가 자동으로 호출이 됨
